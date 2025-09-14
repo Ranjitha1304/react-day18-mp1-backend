1fda2ab04f561bdd009e25a75c0199a12ece3237
Ranjitha1304 <ranjitha13cs@gmail.com>
2025-09-14 23:09:16 +0530

    Update settings.py

--- product_backend/settings.py
+++ product_backend/settings.py
@@ -128,3 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",   # Vite default
+]
+
+CORS_ALLOW_CREDENTIALS = True
